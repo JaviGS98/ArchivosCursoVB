f64a79de793b075b6fcd65587a762efd5ba3c75b
RamonRS <celmon1@gmail.com>
2017-01-11 10:24:16 +0100

    Decidir si un numero es primo con funciones

---
+++ Tema 08 - Funciones/NumeroPrimo/NumeroPrimo/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+    Function contarDivisores(ByVal n As Integer) As Integer
+        Dim contador As Integer = 0
+
+        For i As Integer = 1 To n Step 1
+            If n Mod i = 0 Then
+                contador += 1
+            End If
+        Next
+        Return contador
+    End Function
+    Sub Main()
+
+    End Sub
+
+End Module
