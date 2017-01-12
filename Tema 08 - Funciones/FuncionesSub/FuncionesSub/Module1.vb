7ec468acbd3f3b11718363fa840469fc990588d9
RamonRS <celmon1@gmail.com>
2017-01-12 12:09:38 +0100

    Funciones Sub

---
+++ Tema 08 - Funciones/FuncionesSub/FuncionesSub/Module1.vb
@@ -0,0 +1,54 @@
+﻿Option Strict On
+Module Module1
+
+    Sub calcularSuma(ByVal x As Integer, ByVal y As Integer, ByRef suma As Integer)
+
+        suma = x + y
+
+    End Sub
+
+    Sub saludar(ByVal nombre As String)
+        Console.WriteLine("Hola " & nombre)
+    End Sub
+
+    Function devuelveSaludo(ByVal nombre As String) As String
+        Return "Hola " + nombre
+    End Function
+
+    Public Sub factorial(ByVal num As Integer, ByRef fact As Long)
+        If num = 0 Then
+            fact = 1
+        Else
+            fact = 1
+            While num > 0
+                fact = fact * num
+                num = num - 1
+            End While
+        End If
+
+    End Sub
+
+    Sub Main()
+        Dim num1 As Integer = 2
+        Dim num2 As Integer = 13
+        Dim s As Integer
+        calcularSuma(num1, num2, s)
+        saludar("Alejandro")
+        Console.WriteLine(devuelveSaludo("Brais"))
+        Console.WriteLine(s)
+        Console.ReadLine()
+
+        Console.WriteLine("Hola")
+        Dim n As String = Console.ReadLine()
+
+        Dim f As Long
+        Dim numero As Integer = 5
+        factorial(numero, f)
+        Console.WriteLine("El factorial de 5 es : " & f)
+        f = 0
+        factorial(20, f)
+        Console.WriteLine("El factorial de 20 es : " & f)
+        Console.ReadLine()
+       End Sub
+
+End Module
---
+++ Tema 08 - Funciones/ValorVsReferencia/ValorVsReferencia/Module1.vb
@@ -0,0 +1,44 @@
+﻿Option Strict On
+Module Module1
+    Function sumar10(ByVal x As Integer) As Integer
+        x = x + 10
+        Return x
+    End Function
+
+    Sub sumar10Referencia(ByRef x As Integer)
+        x = x + 10
+    End Sub
+
+    Sub modificArray(ByVal m() As Integer)
+        m(0) = 0
+        m(m.Length - 1) = 0
+    End Sub
+    Sub Main()
+        Dim numero As Integer = 20
+
+        Console.WriteLine("Muestro el resultado de la funcion sumar10:")
+
+        Console.WriteLine(sumar10(numero))
+        Console.WriteLine("Muestro el valor de la variable numero:")
+        Console.WriteLine(numero)
+
+        'Si queremos modificar la variable original
+        numero = 1
+        sumar10Referencia(numero)
+        Console.WriteLine(numero)
+
+        Dim prueba() As Integer = {4, 3, 2, 1, 7}
+        modificArray(prueba)
+        Console.WriteLine("Matriz modificada")
+        For Each m As Integer In prueba
+            Console.WriteLine(m)
+        Next
+
+
+
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
