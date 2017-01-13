2c9bc126f1b0c6400927fe1c5651719929b8605b
RamonRS <celmon1@gmail.com>
2017-01-13 09:35:18 +0100

    Actualizacion

---
+++ Tema 08 - Funciones/Boletin/Ejercicio6/Ejercicio6/Module1.vb
@@ -0,0 +1,43 @@
+﻿Option Strict On
+Module Module1
+
+    Sub intercambiar(ByRef x As Integer, ByRef y As Integer)
+        Dim z As Integer = x
+        x = y
+        y = z
+    End Sub
+
+    Function sumar10(ByVal x As Integer) As Integer
+        Return x + 10
+    End Function
+
+    Sub sumar10Referencia(ByRef x As Integer)
+        x = x + 10
+    End Sub
+
+
+    Sub Main()
+        Dim n1 As Integer = 5
+        Dim n2 As Integer = 3
+
+        intercambiar(n1, n2)
+
+        Console.WriteLine("n1 = " & n1)
+        Console.WriteLine("n2 = " & n2)
+
+
+        Dim numero As Integer = 1
+        numero = sumar10(numero)
+
+        sumar10Referencia(numero)
+
+
+
+
+
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ Tema 08 - Funciones/Boletin/Ejercicio8/Ejercicio8/Module1.vb
@@ -0,0 +1,22 @@
+﻿Option Strict On
+Module Module1
+    Sub sumaDivisores(ByVal n As Integer, ByRef suma As Integer)
+        suma = 0
+        For i As Integer = 1 To n Step 1
+            If n Mod i = 0 Then
+                suma = suma + i
+            End If
+        Next
+    End Sub
+
+    Sub Main()
+        Dim n As Integer = 110
+        Dim sum As Integer
+
+        sumaDivisores(n, sum)
+        Console.WriteLine("La suma de los divisores es : " & sum)
+
+        Console.ReadLine()
+    End Sub
+
+End Module
