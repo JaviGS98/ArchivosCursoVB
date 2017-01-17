3f08c0d08f94511e854049df82b12614fb4e3a85
RamonRS <celmon1@gmail.com>
2017-01-17 14:52:40 +0100

    Act

---
+++ Tema 08 - Funciones/Boletin/Ejercicio21.vb
@@ -0,0 +1,22 @@
+﻿Option Strict On
+Module Module1
+    Function contarPosicionesNoVacias(ByRef m() As String) As Integer
+        Dim contador As Integer
+
+        For i As Integer = 0 To m.Length - 1 Step 1
+            If Not m(i) Is Nothing Then
+                contador = contador + 1
+            End If
+        Next
+
+        Return contador
+    End Function
+    Sub Main()
+        Dim matriz() As String = {"Pepe", "Juan"}
+
+        Console.WriteLine("Strings guardados : " & contarPosicionesNoVacias(matriz))
+
+        Console.ReadLine()
+    End Sub
+
+End Module
