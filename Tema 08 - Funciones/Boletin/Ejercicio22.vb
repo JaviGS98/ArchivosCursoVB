b31489ed2f148bfcf78f7d55e571b42401465597
RamonRS <celmon1@gmail.com>
2017-01-17 15:06:39 +0100

    Act

---
+++ Tema 08 - Funciones/Boletin/Ejercicio22.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+    Function buscarString(ByRef n() As String, ByRef cadenan As String) As Boolean
+        Dim encontrado As Boolean
+
+        For i As Integer = 0 To n.Length - 1 Step 1
+            If n(i) = cadenan Then
+                encontrado = True
+                Exit For
+            End If
+        Next
+        Return encontrado
+    End Function
+    Sub Main()
+        Dim matriz() As String = {"PEpe", "Juan", "Zoel"}
+
+        If buscarString(matriz, "Zoel") Then
+            Console.WriteLine("Zoel está en la matriz")
+        Else
+            Console.WriteLine("No se ha encontrado")
+        End If
+
+    End Sub
+
+End Module
