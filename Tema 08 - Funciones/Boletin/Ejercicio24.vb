5c8f8b2b66ed96f457343b7f542d1323e00ad3d2
RamonRS <celmon1@gmail.com>
2017-01-19 08:55:06 +0100

    Ejercicio24

---
+++ Tema 08 - Funciones/Boletin/Ejercicio24.vb
@@ -0,0 +1,28 @@
+﻿Option Strict On
+Module Module1
+    Sub eliminar(ByRef matriz() As String, ByRef cadena As String)
+
+        For i As Integer = 0 To matriz.Length - 1 Step 1
+            If matriz(i) = cadena Then
+                matriz(i) = Nothing
+                'Exit For
+            End If
+        Next
+    End Sub
+    Sub Main()
+        Dim matriz() As String = {"1234", "6754", "1234", "3456"}
+
+        eliminar(matriz, "1234")
+
+        For Each cadena As String In matriz
+            If cadena Is Nothing Then
+                Console.WriteLine("VACIO")
+            Else
+                Console.WriteLine(cadena)
+            End If
+        Next
+
+        Console.ReadLine()
+    End Sub
+
+End Module
