fa15ce5e61f9c14578b2032718ffa2a90163bc0b
RamonRS <celmon1@gmail.com>
2017-01-19 11:31:54 +0100

    Aleatorios

---
+++ Tema 08 - Funciones/Boletin/Ejercicio12.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+    Sub llenarMatriz(ByRef m() As Integer)
+
+        Dim aleatorio As Random = New Random()
+
+        For i As Integer = 0 To m.Length - 1 Step 1
+            m(i) = aleatorio.Next(20)
+        Next
+
+
+    End Sub
+
+    Sub mostrarPorConsola(ByRef m() As Integer)
+        For Each numero As Integer In m
+            Console.Write(numero & " ")
+        Next
+    End Sub
+    Sub Main()
+        Dim matriz(2999) As Integer
+
+        llenarMatriz(matriz)
+
+        mostrarPorConsola(matriz)
+
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
