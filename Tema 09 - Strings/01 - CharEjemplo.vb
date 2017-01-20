ca9e085b66f560ba8b34e5c12ee59ce696ade472
RamonRS <celmon1@gmail.com>
2017-01-20 11:10:25 +0100

    Ejemplo Char

---
+++ Tema 09 - Strings/01 - CharEjemplo.vb
@@ -0,0 +1,28 @@
+﻿Option Strict On
+Module Module1
+
+    Sub Main()
+        Dim caracter As Char = "&"c
+
+        If Char.IsNumber(caracter) Then
+            Console.WriteLine("Es un número")
+        ElseIf Char.IsLetter(caracter) Then
+
+            Console.WriteLine("Es una letra")
+        Else
+            Console.WriteLine("No es ni un número ni una letra")
+        End If
+
+        'Convertir en mayúscula
+        Dim letra As Char = "o"c
+
+        If Char.IsLower(letra) Then
+            letra = Char.ToUpper(letra)
+        End If
+
+        Console.WriteLine(letra)
+
+        Console.ReadLine()
+    End Sub
+
+End Module
