95464ed1153e74c8fe7477ac55c467483b8e2a09
RamonRS <celmon1@gmail.com>
2017-01-25 09:58:33 +0100

    Repaso

---
+++ Tema 09 - Strings/03 - RepasoFunciones.vb
@@ -0,0 +1,37 @@
+﻿Option Strict On
+Module Module1
+
+    Sub Main()
+        Dim frase As String = "Probando la clase String que locura"
+        Dim caracterPosicionCero As Char = frase(0)
+
+        'Encontrar las posiciones del caracter a:
+        Console.WriteLine("La letra a está en : ")
+        For i As Integer = 0 To frase.Length - 1 Step 1
+            If frase(i) = "a"c Then
+                Console.WriteLine("Posición: " & i)
+            End If
+        Next
+
+        'Encontrar la posición del primer espacio
+        Dim pos As Integer = frase.IndexOf(" "c)
+
+        'Obtener string desde el último espacio hasta el final
+        Dim ultimaPosicion As Integer = frase.LastIndexOf(" "c)
+        Dim palabra As String = frase.Substring(ultimaPosicion + 1)
+        Console.WriteLine(palabra)
+
+        'Cuantas palabras empiezna por l
+        Dim palabras() As String = frase.Split(" "c)
+        Dim contador As Integer = 0
+        For Each p As String In palabras
+            If p.StartsWith("l") Then
+                contador += 1
+            End If
+        Next
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
