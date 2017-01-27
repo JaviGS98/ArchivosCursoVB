34d42b4f60ad87e26c74b84929c8e8eefda676ef
RamonRS <celmon1@gmail.com>
2017-01-27 11:08:46 +0100

    Actualizacion

---
+++ Tema 08 - Funciones/Boletin/15 - Sumar10AMatriz.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+    Sub sumar10(ByRef m() As Integer)
+        For i As Integer = 0 To m.Length - 1 Step 1
+            m(i) = m(i) + 10
+
+        Next
+    End Sub
+    Sub escribirMatrizPorConsola(ByRef m() As Integer)
+        For Each n As Integer In m
+            Console.Write(n & " ")
+        Next
+    End Sub
+    Sub Main()
+
+        Dim matriz() As Integer = {5, 4, 3, 1}
+        sumar10(matriz)
+
+        escribirMatrizPorConsola(matriz)
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ Tema 08 - Funciones/Boletin/17 - MatrizNegativos.vb
@@ -0,0 +1,42 @@
+﻿Module Module1
+    Function contarNumerosNegativos(ByRef m() As Integer) As Integer
+        Dim c As Integer = 0
+        For Each n As Integer In m
+            If n < 0 Then
+                c += 1
+            End If
+        Next
+
+        Return c
+    End Function
+    Function getNumerosNegativos(ByRef m() As Integer) As Integer()
+        'Necesito saber cuantos números negativos hay para crear 
+        'una matriz del mismo tamaño
+        'Contar números negativos
+        Dim contadorNegativos As Integer = contarNumerosNegativos(m)
+        Dim numerosNegativos(contadorNegativos - 1) As Integer
+        Dim j As Integer = 0
+        'Busco negativos y los guardo en la matriz numerosNegativos
+        For i As Integer = 0 To m.Length - 1 Step 1
+            If m(i) < 0 Then
+                numerosNegativos(j) = m(i)
+                j += 1
+            End If
+        Next
+
+        Return numerosNegativos
+    End Function
+
+
+    Sub Main()
+
+        Dim matriz() As Integer = {-5, 4, -3, 1}
+        Dim negativos() As Integer = getNumerosNegativos(matriz)
+
+        For Each n As Integer In negativos
+            Console.WriteLine(n)
+        Next
+        Console.ReadLine()
+    End Sub
+
+End Module
