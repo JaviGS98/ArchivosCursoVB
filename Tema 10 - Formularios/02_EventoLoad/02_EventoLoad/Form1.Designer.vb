ddcd33b93dfc2315b259844621c6a590d820b889
RamonRS <celmon1@gmail.com>
2017-02-07 11:33:18 +0100

    Forms

---
+++ Tema 10 - Formularios/02_EventoLoad/02_EventoLoad/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPulsar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnPulsar
+        '
+        Me.btnPulsar.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.btnPulsar.Location = New System.Drawing.Point(94, 228)
+        Me.btnPulsar.Name = "btnPulsar"
+        Me.btnPulsar.Size = New System.Drawing.Size(89, 39)
+        Me.btnPulsar.TabIndex = 0
+        Me.btnPulsar.Text = "Pulsar"
+        Me.btnPulsar.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(754, 347)
+        Me.Controls.Add(Me.btnPulsar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnPulsar As Button
+End Class
---
+++ Tema 10 - Formularios/02_EventoLoad/02_EventoLoad/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    'Zona de declaración de variables globales al formulario
+    Dim prueba As String
+    Dim edad As Integer
+    'Función manejadora evento load: Aquí introducimos código
+    'para que se ejecute al arrancar la aplicación
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Podríamos inicializar aquí las variables:
+        prueba = "Mensaje de bienvenida"
+        edad = 24
+        MessageBox.Show(prueba)
+
+    End Sub
+
+    Private Sub btnPulsar_Click(sender As Object, e As EventArgs) Handles btnPulsar.Click
+        MessageBox.Show("Botón pulsado")
+    End Sub
+
+    Private Sub btnPulsar_MouseMove(sender As Object, e As MouseEventArgs) Handles btnPulsar.MouseMove
+        btnPulsar.BackColor = Color.Coral
+    End Sub
+
+    Private Sub btnPulsar_MouseLeave(sender As Object, e As EventArgs) Handles btnPulsar.MouseLeave
+        btnPulsar.BackColor = Color.Gainsboro
+    End Sub
+End Class
