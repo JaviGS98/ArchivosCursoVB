66c4768f6126292b136dafdc1822c973deca1eb1
RamonRS <celmon1@gmail.com>
2017-02-20 16:50:47 +0100

    Forms

---
+++ Tema 10 - Formularios/BolAppGraficasEj6/BolAppGraficasEj6/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCentigrados = New System.Windows.Forms.TextBox()
+        Me.txtFahrenheit = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnConvertir = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtCentigrados
+        '
+        Me.txtCentigrados.Location = New System.Drawing.Point(143, 92)
+        Me.txtCentigrados.Name = "txtCentigrados"
+        Me.txtCentigrados.Size = New System.Drawing.Size(100, 20)
+        Me.txtCentigrados.TabIndex = 0
+        '
+        'txtFahrenheit
+        '
+        Me.txtFahrenheit.Location = New System.Drawing.Point(143, 142)
+        Me.txtFahrenheit.Name = "txtFahrenheit"
+        Me.txtFahrenheit.Size = New System.Drawing.Size(100, 20)
+        Me.txtFahrenheit.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 92)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Grados Centígrados"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 142)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Grados Fahrenheit"
+        '
+        'btnConvertir
+        '
+        Me.btnConvertir.Location = New System.Drawing.Point(428, 28)
+        Me.btnConvertir.Name = "btnConvertir"
+        Me.btnConvertir.Size = New System.Drawing.Size(105, 32)
+        Me.btnConvertir.TabIndex = 4
+        Me.btnConvertir.Text = "Convertir"
+        Me.btnConvertir.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(357, 151)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(165, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(588, 261)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnConvertir)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtFahrenheit)
+        Me.Controls.Add(Me.txtCentigrados)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtCentigrados As TextBox
+    Friend WithEvents txtFahrenheit As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnConvertir As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Tema 10 - Formularios/BolAppGraficasEj6/BolAppGraficasEj6/Form1.vb
@@ -0,0 +1,56 @@
+﻿Option Strict On
+Public Class Form1
+
+    'Declaración variables
+    Dim gradosCentigrados, gradosFahrenheit As Double
+
+    Private Sub txtCentigrados_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtCentigrados.KeyPress, txtFahrenheit.KeyPress
+
+        If e.KeyChar = Convert.ToChar(Keys.Enter) Then
+            'Obtengo el control que generó el evento
+            Dim caja As TextBox = CType(sender, TextBox)
+
+            If caja.Name = "txtCentigrados" Then
+                gradosCentigrados = Convert.ToDouble(txtCentigrados.Text)
+                gradosFahrenheit = gradosCentigrados + 10
+
+                txtFahrenheit.Text = gradosFahrenheit.ToString()
+            Else
+                gradosFahrenheit = Convert.ToDouble(txtFahrenheit.Text)
+                gradosCentigrados = gradosFahrenheit - 10
+
+                txtCentigrados.Text = gradosCentigrados.ToString
+            End If
+        End If
+
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If e.KeyChar = Convert.ToChar(Keys.Enter) Then
+
+        End If
+    End Sub
+
+    Private Sub btnConvertir_Click(sender As Object, e As EventArgs) Handles btnConvertir.Click
+
+        'Comprobar si hay texto en caja 1
+        If txtCentigrados.Text <> "" Then
+            gradosCentigrados = Convert.ToDouble(txtCentigrados.Text)
+            gradosFahrenheit = gradosCentigrados + 10
+
+            txtFahrenheit.Text = gradosFahrenheit.ToString()
+            txtCentigrados.Text = ""
+
+
+
+        ElseIf txtFahrenheit.Text <> "" Then
+            gradosFahrenheit = Convert.ToDouble(txtFahrenheit.Text)
+            gradosCentigrados = gradosFahrenheit - 10
+
+            txtCentigrados.Text = gradosCentigrados.ToString
+            txtFahrenheit.Text = ""
+
+        End If
+    End Sub
+End Class
