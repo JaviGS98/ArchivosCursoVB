47c26d754c500e941dfae6749415f2ad11191076
ramonrsgithub <celmon1@gmail.com>
2017-02-28 06:45:32 +0100

    Tema 11 Forms

---
+++ Tema 11 - Formularios(II)/01-ListBox/01-ListBox/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnAddItem = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnGetItemSelected = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"Ourense", "Pontevedra", "Lugo", "Santiago", "Ferrol", "A Coruña", "Vigo", "Monforte", "Padrón", "Redondela", "Xinzo de Limia"})
+        Me.ListBox1.Location = New System.Drawing.Point(84, 54)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(162, 160)
+        Me.ListBox1.TabIndex = 0
+        '
+        'btnAddItem
+        '
+        Me.btnAddItem.Location = New System.Drawing.Point(91, 338)
+        Me.btnAddItem.Name = "btnAddItem"
+        Me.btnAddItem.Size = New System.Drawing.Size(104, 39)
+        Me.btnAddItem.TabIndex = 1
+        Me.btnAddItem.Text = "Añadir elemento"
+        Me.btnAddItem.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(91, 301)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(164, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(88, 268)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(116, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Escribir ciudad gallega:"
+        '
+        'btnGetItemSelected
+        '
+        Me.btnGetItemSelected.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGetItemSelected.Location = New System.Drawing.Point(303, 54)
+        Me.btnGetItemSelected.Name = "btnGetItemSelected"
+        Me.btnGetItemSelected.Size = New System.Drawing.Size(114, 54)
+        Me.btnGetItemSelected.TabIndex = 4
+        Me.btnGetItemSelected.Text = "Obtener elemento seleccionado"
+        Me.btnGetItemSelected.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(916, 449)
+        Me.Controls.Add(Me.btnGetItemSelected)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnAddItem)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents btnAddItem As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnGetItemSelected As Button
+End Class
---
+++ Tema 11 - Formularios(II)/01-ListBox/01-ListBox/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub btnAddItem_Click(sender As Object, e As EventArgs) Handles btnAddItem.Click
+        Dim ciudad As String = TextBox1.Text
+
+        ListBox1.Items.Add(ciudad)
+    End Sub
+
+    Private Sub btnGetItemSelected_Click(sender As Object, e As EventArgs) Handles btnGetItemSelected.Click
+        Dim elemento As String = ListBox1.SelectedItem.ToString
+        MessageBox.Show("Ciudad seleccionada: " & elemento)
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Dim elemento As String = ListBox1.SelectedItem.ToString
+        MessageBox.Show("Ciudad seleccionada: " & elemento)
+    End Sub
+End Class
