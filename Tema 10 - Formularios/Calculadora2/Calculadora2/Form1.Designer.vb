8b1f4991b2d3eab859f87da239520825be77644f
RamonRS <celmon1@gmail.com>
2017-03-03 09:48:19 +0100

    Calculadora

---
+++ Tema 10 - Formularios/Calculadora2/Calculadora2/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnMas = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.btnIgual = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(37, 55)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(124, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(66, 132)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnMas
+        '
+        Me.btnMas.Location = New System.Drawing.Point(66, 184)
+        Me.btnMas.Name = "btnMas"
+        Me.btnMas.Size = New System.Drawing.Size(75, 23)
+        Me.btnMas.TabIndex = 1
+        Me.btnMas.Text = "+"
+        Me.btnMas.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(147, 132)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "2"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(228, 132)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 1
+        Me.Button4.Text = "3"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'btnIgual
+        '
+        Me.btnIgual.Location = New System.Drawing.Point(157, 184)
+        Me.btnIgual.Name = "btnIgual"
+        Me.btnIgual.Size = New System.Drawing.Size(75, 23)
+        Me.btnIgual.TabIndex = 1
+        Me.btnIgual.Text = "="
+        Me.btnIgual.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(66, 226)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "-"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(410, 261)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btnIgual)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnMas)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnMas As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents btnIgual As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Tema 10 - Formularios/Calculadora2/Calculadora2/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Dim numero1, numero2, resultado As Double
+    Dim operacion As Char
+
+    Private Sub btnIgual_Click(sender As Object, e As EventArgs) Handles btnIgual.Click
+        numero2 = TextBox1.Text
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button3.Click, Button4.Click
+        'Obtener el botón que ha sido pulsado
+        Dim b As Button = CType(sender, Button)
+        TextBox1.Text += b.Text
+    End Sub
+
+    Private Sub btnMas_Click(sender As Object, e As EventArgs) Handles btnMas.Click
+        numero1 = Convert.ToDouble(TextBox1.Text)
+        TextBox1.Text = ""
+        operacion = "+"c
+    End Sub
+End Class
