8835ff5c17f45e6ab5e6ae06c329805a5aa96c49
RamonRS <celmon1@gmail.com>
2017-04-21 12:34:19 +0200

    h

--- Tema 12 - Objetos/02-Ficheros/02-Ficheros/Module1.vb
+++ Tema 12 - Objetos/02-Ficheros/02-Ficheros/Module1.vb
@@ -2,48 +2,64 @@
 Module Module1
 
     Sub Main()
+        'Dim v As Vuelo = New Vuelo()
+
+        'v.setCodigo("AW34")
+        'v.codigoAvion = "Airbus7893"
+        'v.fecha = New Date(2017, 3, 20)
+        'v.plazasLibres = 290
+
+        'Dim escritor As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter("miArchivo.txt", True)
+
+        'escritor.WriteLine(v.getCodigo() & "*" & v.codigoAvion & "*" & v.fecha.ToShortDateString & "*" & v.plazasLibres)
+        'escritor.Close()
+
+        ''Para recuperar vuelos del fichero
+
+        'Dim vuelos(9) As Vuelo
+        'Dim i As Integer = 0
+        'Dim lector As StreamReader = My.Computer.FileSystem.OpenTextFileReader("miArchivo.txt")
+
+        'While Not lector.EndOfStream
+        '    If vuelos(i) Is Nothing Then
+        '        'Reconstruir objeto a partir de String
+        '        Dim propiedadesVuelo() As String = lector.ReadLine().Split("*")
+        '        Dim vuelo As Vuelo = New Vuelo()
+        '        vuelo.codigo = propiedadesVuelo(0)
+        '        vuelo.codigoAvion = propiedadesVuelo(1)
+        '        vuelo.fecha = New Date() 'Falta reconstruir la fecha
+
+        '        vuelo.plazasLibres = propiedadesVuelo(3)
+
+        '        vuelos(i) = vuelo
+        '        i += 1
+        '    End If
+        'End While
+
+        ''Comprobación
+
+        'For Each vuelo As Vuelo In vuelos
+        '    If Not vuelo Is Nothing Then
+        '        Console.WriteLine(vuelo.codigo)
+
+        '    End If
+        'Next
+
+
+
+        'Crear vuelos
         Dim v As Vuelo = New Vuelo()
+        Dim v2 As Vuelo = New Vuelo("ER34", "Airbus456", New Date(2017, 4, 21), 450)
 
-        v.codigo = "AW34"
-        v.codigoAvion = "Airbus7893"
+        'dar valores a v
+        v.setCodigo("WE3")
+        v.codigoAvion = "Boeing708"
         v.fecha = New Date(2017, 3, 20)
-        v.plazasLibres = 290
+        v.plazasLibres = 300
+        'Comprobar que v2 está correcto
+        Console.WriteLine(v2)
+        Console.ReadLine()
 
-        Dim escritor As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter("miArchivo.txt", True)
-
-        escritor.WriteLine(v.codigo & "*" & v.codigo & "*" & v.fecha.ToShortDateString & "*" & v.plazasLibres)
-        escritor.Close()
-
-        'Para recuperar vuelos del fichero
-
-        Dim vuelos(9) As Vuelo
-        Dim i As Integer = 0
-        Dim lector As StreamReader = My.Computer.FileSystem.OpenTextFileReader("miArchivo.txt")
-
-        While Not lector.EndOfStream
-            If vuelos(i) Is Nothing Then
-                'Reconstruir objeto a partir de String
-                Dim propiedadesVuelo() As String = lector.ReadLine().Split("*")
-                Dim vuelo As Vuelo = New Vuelo()
-                vuelo.codigo = propiedadesVuelo(0)
-                vuelo.codigoAvion = propiedadesVuelo(1)
-                vuelo.fecha = New Date() 'Falta reconstruir la fecha
-
-                vuelo.plazasLibres = propiedadesVuelo(3)
-
-                vuelos(i) = vuelo
-                i += 1
-            End If
-        End While
-
-        'Comprobación
-
-        For Each vuelo As Vuelo In vuelos
-            If Not vuelo Is Nothing Then
-                Console.WriteLine(vuelo.codigo)
-
-            End If
-        Next
 
         Console.ReadLine()
     End Sub
--- Tema 12 - Objetos/02-Ficheros/02-Ficheros/Vuelo.vb
+++ Tema 12 - Objetos/02-Ficheros/02-Ficheros/Vuelo.vb
@@ -1,14 +1,46 @@
 ﻿Public Class Vuelo
-    Public codigo As String
+    Private codigo As String
     Public codigoAvion As String
     Public fecha As Date
     Public plazasLibres As Short
 
     'Constructor por defecto
+    Public Sub New()
+
+    End Sub
+
     'Constructor que recibe todos los argumentos
-
+    Public Sub New(codigo As String, codigoAvion As String, fecha As Date, plazas As Short)
+        'Validar que el código tiene un dígito: llamamos a la 
+        'función setCodigo programada más abajo
+        setCodigo(codigo)
+        Me.codigoAvion = codigoAvion
+        Me.fecha = fecha
+        Me.plazasLibres = plazas
+    End Sub
     'Sobreescribir el toString
-
-    'Controlar que el código del vuelo tenga al menos un dígito
+    Public Overrides Function ToString() As String
+        Return Me.codigo + "*" + Me.codigoAvion + "*" + Me.fecha.ToShortDateString + "*" + Me.plazasLibres.ToString()
+    End Function
     'Añadir funciones de acceso para el código
+    'Controlar que el código del vuelo tenga al menos un dígito
+    'set: codigo
+    Public Sub setCodigo(c As String)
+        Dim contador As Integer
+        For i As Integer = 0 To c.Length - 1 Step 1
+            If Char.IsDigit(c(i)) Then
+                contador += 1
+                Exit For
+            End If
+        Next
+
+        If contador > 0 Then
+            Me.codigo = c
+        End If
+    End Sub
+
+    Public Function getCodigo() As String
+        Return Me.codigo
+    End Function
+
 End Class
