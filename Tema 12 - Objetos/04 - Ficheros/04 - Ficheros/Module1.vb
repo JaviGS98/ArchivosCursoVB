d54c3cb213f623ded589ab416168eed421f166a5
RamonRS <celmon1@gmail.com>
2017-04-26 09:54:43 +0200

    Sniper

---
+++ Tema 12 - Objetos/04 - Ficheros/04 - Ficheros/Module1.vb
@@ -0,0 +1,52 @@
+﻿Option Strict On
+Imports System.IO
+Module Module1
+
+    Sub Main()
+        Dim sniper As Sniper = New Sniper("12e1243", "Chris Kyle", 200, 0.9)
+        'Guardar en fichero
+        Dim ruta As String = Directory.GetCurrentDirectory() + "/snipers.txt"
+        Dim escritor As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(ruta, True)
+
+        'Semi-profesional:
+        escritor.WriteLine(sniper.guardarFichero())
+        'Beginner:
+        'escritor.WriteLine(sniper.codigo + "*" + sniper.nombre + "*" + sniper.balas.ToString() + "*" + sniper.precision.ToString())
+        escritor.Close()
+        Console.WriteLine(sniper.guardarFichero())
+
+
+        ' Leer registros del fichero y pasarlos a matriz de Snipers
+        Dim misSnipers(9) As Sniper
+        Dim lector As StreamReader = My.Computer.FileSystem.OpenTextFileReader(ruta)
+        Dim i As Integer = 0
+        While Not lector.EndOfStream
+            '1 - Splittear registro
+            Dim tokens() As String = lector.ReadLine().Split("*"c)
+            '2 - Crear un sniper
+            Dim s As Sniper = New Sniper()
+            s.codigo = tokens(0)
+            s.nombre = tokens(1)
+            s.balas = Convert.ToInt32(tokens(2))
+            s.precision = Convert.ToSingle(tokens(3))
+            '3-Guardarlo en la matriz
+            If misSnipers(i) Is Nothing Then
+                misSnipers(i) = s
+            End If
+            i += 1
+        End While
+        lector.Close()
+
+        'Comprobar matriz snipers
+        For Each sni As Sniper In misSnipers
+            If Not sni Is Nothing Then
+                Console.WriteLine(sni)
+            Else
+                Exit For
+            End If
+
+        Next
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ Tema 12 - Objetos/04 - Ficheros/04 - Ficheros/Sniper.vb
@@ -0,0 +1,26 @@
+﻿Option Strict On
+Public Class Sniper
+    Public codigo As String
+    Public nombre As String
+    Public balas As Integer
+    Public precision As Single
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(codigo As String, nombre As String, balas As Integer, precision As Single)
+        Me.codigo = codigo
+        Me.nombre = nombre
+        Me.balas = balas
+        Me.precision = precision
+    End Sub
+
+    Public Function guardarFichero() As String
+        Return Me.codigo + "*" + Me.nombre + "*" + Me.balas.ToString() + "*" + Me.precision.ToString()
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return " Código: " + Me.codigo + " Nombre: " + Me.nombre + " Balas: " + balas.ToString() + " Precision: " + precision.ToString()
+    End Function
+End Class
